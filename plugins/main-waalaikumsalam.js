a27431a474196cc283f29f7f334f1de12ebb63c4
KINGBOTs1 <cheatgameden@gmail.com>
2022-11-03 08:40:29 +0800

    Create main-waalaikumsalam.js

---
+++ plugins/main-waalaikumsalam.js
@@ -0,0 +1,10 @@
+let handler = async (m, ) => {
+
+    m.reply('Waalaikumsalam')
+}
+
+handler.customPrefix = /^(assalam(ualaikum)?|(salamualaiku|(sa(lamu|m)liku|sala))m)$/i
+handler.command = new RegExp
+
+
+export default handler
