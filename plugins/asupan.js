00fced313d887dcc8404c576e6dbf2839f04b48c
KINGBOTs1 <cheatgameden@gmail.com>
2022-12-01 03:49:52 +0800

    Update asupan.js

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch' 
+import db from '../lib/database.js'
 import moment from 'moment-timezone'
  let handler = async(m, { conn, usedPrefix, args, command }) => { 
      let name = db.data.users[m.sender].name 
