938806977f707b388f4d391828f2270b91e96111
KINGBOTs1 <cheatgameden@gmail.com>
2022-12-02 06:41:44 +0800

    Update nsfw-viral.js

--- plugins/nsfw-viral.js
+++ plugins/nsfw-viral.js
@@ -2,7 +2,6 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
-if (global.db.data.users[m.sender].premiumTime < 1) return conn.reply(m.chat, '𝐅𝐈𝐓𝐔𝐑 𝐈𝐍𝐈 𝐊𝐇𝐔𝐒𝐔𝐒 𝐏𝐑𝐄𝐌𝐈𝐔𝐌!!', m)
       let _fun = JSON.parse(fs.readFileSync('./json/viral.json'))
       let json = _fun[Math.floor(Math.random() * _fun.length)]
       let caption = `❏  *RANDOM - VIRAL*\n\n`
